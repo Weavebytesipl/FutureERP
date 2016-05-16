14daa7c2180dd0e95784b5bb0b8137d66f95bc42
neobytes <dev.kulwantsingh@gmail.com>
2016-05-16 13:16:21 +0530

    adding payment admin model

--- pos/admin.py
+++ pos/admin.py
@@ -4,7 +4,11 @@ from pos.models import ProductInTransaction, Payment, PaymentMethod, Transaction
 
 admin.site.register(SalesOutlet)
 admin.site.register(ProductInTransaction)
-admin.site.register(Payment)
 admin.site.register(PaymentMethod)
 admin.site.register(Transaction)
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('method', 'transaction', 'amount', 'other_details')
+
+admin.site.register(Payment, PaymentAdmin)
 
