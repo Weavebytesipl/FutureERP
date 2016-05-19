264c1018fd26677fbcb75b7d94b3fb25172b524c
neobytes <dev.kulwantsingh@gmail.com>
2016-05-19 13:55:09 +0530

    adding helpdesk models, amdin, migrations etc

--- helpdesk/admin.py
+++ helpdesk/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from helpdesk.models import Complaint, Supporter
 
 # Register your models here.
+
+admin.site.register(Complaint)
+admin.site.register(Supporter)
--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,5 +1,62 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Room(models.Model):
+    """
+    model for a chat room
+    """
+    name = models.CharField(max_length=64)
+    created_by = models.ForeignKey(User)
+    active = models.BooleanField(_('Active'), default=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created'))
+
+    def __unicode__(self):
+        return self.name
+
+
+class Message(models.Model):
+	"""
+	message model to store a chat message
+	"""
+	content = models.TextField()
+	sender = models.ForeignKey(User, related_name='sender_id')
+	receiver = models.ForeignKey(User, related_name='receiver_id')
+	room = models.ForeignKey(Room)
+	created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created'))
+
+	def __unicode__(self):
+		return self.content[:30]
+
+
+class Complaint(models.Model):
+    """
+    complaint model for help desk
+    """
+    title = models.CharField(max_length=64, verbose_name=_('Title'))
+    detail = models.TextField(verbose_name=_('Detail'))
+    sender = models.ForeignKey(User, related_name='Sender')
+    to = models.ForeignKey(User, related_name='To')
+    date = models.DateTimeField(auto_now_add=True, verbose_name=_('Date'))
+
+    def __unicode__(self):
+        return self.title
+
+
+class Supporter(models.Model):
+    """
+    support model for the help desk
+    """
+    user = models.OneToOneField(User)
+    # is available for handling complaint
+    is_avail = models.BooleanField(default=False, verbose_name=_('Busy'))
+
+    def __unicode__(self):
+        return "%s" % self.user
+
+    class Meta:
+        verbose_name_plural = "supporter"    
+
